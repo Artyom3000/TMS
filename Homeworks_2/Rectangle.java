11c205f0c04e4d9dbd7337d7394b5974e6b4c4db
Artyom3000 <85163630+Artyom3000@users.noreply.github.com>
2021-06-05 19:38:10 +0300

    Add files via upload

---
+++ Homeworks_2/Rectangle.java
@@ -0,0 +1,29 @@
+package Homeworks_2;
+
+public class Rectangle {
+
+    private Point leftUp, rightDown;
+
+    public Rectangle(Point leftUp, Point rightDown) {
+        this.leftUp = leftUp;
+        this.rightDown = rightDown;
+    }
+
+    public Rectangle() {
+
+    }
+
+    public double Square() {
+        return (leftUp.getY() - rightDown.getY()) * (rightDown.getX() - leftUp.getX());
+    }
+
+    public double Perimeter() {
+        return ((leftUp.getY() - rightDown.getY()) + (rightDown.getX() - leftUp.getX())) * 2;
+    }
+
+    public double Diagonal() {
+        return Math.hypot(leftUp.getY() - rightDown.getY(), rightDown.getX() - leftUp.getX());
+    }
+
+}
+
